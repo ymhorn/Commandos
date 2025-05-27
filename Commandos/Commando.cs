a387c65b4e3d7eea740a7d11babab07333780d67
Y M Horn <ymh53r@gmail.com>
2025-05-27 09:59:21 +0300

    Created class commando

---
+++ Commandos/Commando.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal class Commando
+    {
+        public string Name;
+        public string CodeName;
+        public string[] Utensils = new string[5];
+        public string Status;
+
+        public Commando(string name, string codeName, string[] utensils, string status)
+        {
+            this.Name = name;
+            this.CodeName = codeName;
+            this.Utensils = utensils;
+            this.Status = status;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("The commando is walking.");
+            Status = "Walking";
+        }
+
+        public void Hide()
+        {
+            Console.WriteLine("The commando is hiding.");
+            Status = "Hiding";
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"Commando {CodeName} is attacking.");
+        }
+
+
+    }
+}
